e8535d20c04a0317da84377ae4e07accd9e99e37
Tiancheng Chen <tiancche@cisco.com>
2025-07-08 18:23:10 -0700

    fix(question-card): fix checkbox checker not visible

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -82,7 +82,7 @@ export const QuestionCard: React.FC<QuestionCardProps> = ({
                       : 'border-gray-300'
                   }`}>
                     {isOptionSelected(option.id) && (
-                      <div className="w-2 h-2 bg-white rounded-full mx-auto mt-0.5"></div>
+                      <div className="w-2 h-2 gray rounded-full mx-auto mt-0.5"></div>
                     )}
                   </div>
                 ) : (
@@ -92,7 +92,7 @@ export const QuestionCard: React.FC<QuestionCardProps> = ({
                       : 'border-gray-300'
                   }`}>
                     {isOptionSelected(option.id) && (
-                      <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
+                      <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
                         <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                       </svg>
                     )}
